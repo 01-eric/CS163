963e2e52d17f778196f11044aa8f6282c10c8d32
Eric Fan <eric.113746250@gmail.com>
2018-03-19 13:22:30 -0700

    Update main.cpp

--- BST/main.cpp
+++ BST/main.cpp
@@ -5,7 +5,7 @@ private static void rcPrint(Node<int> node, String tabstop, boolean branch) {
     if (node == null) { // when printing out only left node
         System.out.println(tabstop + "|   ");
         return;
-    } else System.out.println(tabstop + (branch ? "├—— " : "└—— ") + node.getValue());
+    } else System.out.println(tabstop + (branch ? "├── " : "└── ") + node.getValue());
     if (node.getLeft() == null && node.getRight() != null) rcPrint(node.getRight(), tabstop + (branch ? "|   " : "    "), false);
     else if (node.getLeft() != null) {
         rcPrint(node.getRight(), tabstop + (branch ? "|   " : "    "), true);
