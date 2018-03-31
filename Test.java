db71cca2fbe507828af7737e62c1cc14a64d940c
01-eric <eric.113746250@gmail.com>
2018-03-30 22:47:10 -0700

    No commit message

--- Test.java
+++ Test.java
@@ -1,10 +1,11 @@
 import java.util.*;
+import java.io.*;
 
-public class Test {
+/* public class Test {
 	
 	public static void main(String[] args) {
-		System.out.println(caesarCipher("PMGJIJM", 1100, false));
-		System.out.println(vigenereCipher("ur a", "hi", true));
+		System.out.println(caesarCipher("PMG JIJM", 1100, false));
+		System.out.println(vigenereCipher("ur bad", "hi", true));
 	}
 	
 	static String caesarCipher(String s, int offset, boolean encrypt) {
@@ -21,5 +22,35 @@ public class Test {
 			if (Character.isLetter(s.charAt(i))) sb.append((char)((Character.toUpperCase(s.charAt(i)) - 65 + (encrypt ? offset : 26 - (offset % 26))) % 26 + 65));
 		} return sb.toString();
 	}
-}
+} */
 
+
+public class Test {
+	
+	public static void main(String[] args) throws IOException {
+		Scanner scanner = new Scanner(new File("data1.txt"));
+		double[] time = new double[101], speed = new double[101];
+		for (int i = 0; i < time.length; i++) time[i] = Double.parseDouble(scanner.nextLine()) / 3600;
+		for (int i = 0; i < speed.length; i++) speed[i] = Double.parseDouble(scanner.nextLine());
+		System.out.println(trapezoid(time, speed) + "\n" + left(time, speed) + "\n" + right(time, speed));
+	}
+	
+	static double trapezoid(double[] time, double[] speed) {
+		double output = 0;
+		for (int i = 0; i < time.length - 1; i++) output += (time[i + 1] - time[i]) * ((speed[i + 1] + speed[i]) / 2);
+		return output;
+	}
+	
+	static double left(double[] time, double[] speed) {
+		double output = 0;
+		for (int i = 0; i < time.length - 1; i++) output += (time[i + 1] - time[i]) * speed[i];
+		return output;
+	}
+	
+	static double right(double[] time, double[] speed) {
+		double output = 0;
+		for (int i = 1; i < time.length; i++) output += (time[i] - time[i - 1]) * speed[i];
+		return output;
+	}
+	
+}
